ebda27fe62fa87bb209c5569579d6ce53d72d4ba
darkalchemy <darkalchemy@jbmatrix.net>
2018-01-15 05:50:07 +0000

    remove: unneeded ann_config var

--- public/install/extra/ann_config.phpsample.php
+++ public/install/extra/ann_config.phpsample.php
@@ -29,7 +29,6 @@ $site_config['crazy_hour'] = false;
 $site_config['happy_hour'] = false;
 $site_config['ratio_free'] = false;
 
-$site_config['baseurl'] = '#baseurl';
 $site_config['expires']['user_passkey'] = 3600 * 8;
 $site_config['expires']['contribution'] = 3 * 86400;
 $site_config['expires']['happyhour'] = 43200;
