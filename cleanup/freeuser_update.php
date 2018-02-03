eed34f9d7273635937da0ffc6152a58c498da271
darkalchemy <darkalchemy@jbmatrix.net>
2018-02-03 19:42:06 +0000

    fix: freeuser_update.php

--- cleanup/freeuser_update.php
+++ cleanup/freeuser_update.php
@@ -1,43 +1,67 @@
 <?php
+
 /**
  * @param $data
  *
+ * @throws Exception
  * @throws \MatthiasMullie\Scrapbook\Exception\UnbegunTransaction
  */
 function freeuser_update($data)
 {
-    global $site_config, $queries, $cache;
+    global $site_config, $queries, $cache, $fluent;
+
     set_time_limit(1200);
     ignore_user_abort(true);
 
-    $res = sql_query('SELECT id, modcomment FROM users WHERE free_switch > 1 AND free_switch < ' . TIME_NOW) or sqlerr(__FILE__, __LINE__);
-    $msgs_buffer = $users_buffer = [];
-    if (mysqli_num_rows($res) > 0) {
-        $dt = TIME_NOW;
-        $subject = 'Freeleech expired.';
-        $msg = "Your freeleech has expired and has been auto-removed by the system.\n";
-        while ($arr = mysqli_fetch_assoc($res)) {
-            $modcomment = $arr['modcomment'];
-            $modcomment = get_date($dt, 'DATE', 1) . " - Freeleech Removed By System.\n" . $modcomment;
-            $modcom = sqlesc($modcomment);
-            $msgs_buffer[] = '(0,' . $arr['id'] . ',' . $dt . ', ' . sqlesc($msg) . ', ' . sqlesc($subject) . ' )';
-            $users_buffer[] = '(' . $arr['id'] . ', \'0\', ' . $modcom . ')';
-            $cache->update_row('user' . $arr['id'], [
-                'free_switch' => 0,
-                'modcomment'  => $modcomment,
-            ], $site_config['expires']['user_cache']);
-            $cache->increment('inbox_' . $arr['id']);
-        }
-        $count = count($users_buffer);
-        if ($data['clean_log'] && $count > 0) {
-            sql_query('INSERT INTO messages (sender,receiver,added,msg,subject) VALUES ' . implode(', ', $msgs_buffer)) or sqlerr(__FILE__, __LINE__);
-            sql_query('INSERT INTO users (id, free_switch, modcomment) VALUES ' . implode(', ', $users_buffer) . ' ON DUPLICATE KEY UPDATE free_switch = VALUES(free_switch), modcomment = VALUES(modcomment)') or sqlerr(__FILE__, __LINE__);
-        }
-        if ($data['clean_log']) {
-            write_log('Cleanup - Removed Freeleech from ' . $count . ' members');
-        }
-        unset($users_buffer, $msgs_buffer, $count);
+    $query = $fluent->from('users')
+        ->select(null)
+        ->select('id')
+        ->select('modcomment')
+        ->where('free_switch > 1')
+        ->where('free_switch < ?', TIME_NOW);
+
+    $values = $msgs_buffer = $set = [];
+    $dt = TIME_NOW;
+    $subject = 'Freeleech expired.';
+    $msg = "Your freeleech has expired and has been auto-removed by the system.\n";
+    foreach ($query as $arr) {
+        $modcomment = $arr['modcomment'];
+        $modcomment = get_date($dt, 'DATE', 1) . " - Freeleech Removed By System.\n" . $modcomment;
+        $modcom = sqlesc($modcomment);
+        $values[] = [
+            'sender'   => 0,
+            'receiver' => $arr['id'],
+            'added'    => $dt,
+            'msg'      => $msg,
+            'subject'  => $subject,
+        ];
+
+        $set = [
+            'free_switch' => 0,
+            'modcomment'  => $modcom,
+        ];
+
+        $fluent->update('users')
+            ->set($set)
+            ->where('id = ?', $arr['id'])
+            ->execute();
+
+        $cache->update_row('user' . $arr['id'], [
+            'free_switch' => 0,
+            'modcomment'  => $modcomment,
+        ], $site_config['expires']['user_cache']);
+        $cache->increment('inbox_' . $arr['id']);
     }
+    $count = count($values);
+    if ($count > 0) {
+        $fluent->insertInto('messages')
+            ->values($values)
+            ->execute();
+    }
+    if ($data['clean_log']) {
+        write_log('Cleanup - Removed Freeleech from ' . $count . ' members');
+    }
+
     if ($data['clean_log'] && $queries > 0) {
         write_log("Freelech Cleanup: Completed using $queries queries");
     }
