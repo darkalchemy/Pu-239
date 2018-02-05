c58174d4bd89a2f43a42d60af52960d999813559
darkalchemy <darkalchemy@jbmatrix.net>
2018-02-05 22:03:07 +0000

    update: google books api, track api usage and disable if exceeded. cache only book data, not formatting

--- include/function_books.php
+++ include/function_books.php
@@ -9,102 +9,141 @@ use Scriptotek\GoogleBooks\GoogleBooks;
  */
 function get_book_info($torrent)
 {
-    global $cache, $site_config;
+    global $cache, $site_config, $CURUSER;
 
-    $isbn10 = $isbn13 = $poster = '';
     $search = $torrent['name'];
     if (!empty($torrent['isbn'])) {
         $search = $torrent['isbn'];
     }
+    $api_hits = $cache->get('google_api_limits_');
     $hash = hash('sha256', $search);
-    $ebook_info = $cache->get('book_info_' . $hash);
-    if ($ebook_info === false || is_null($ebook_info)) {
+    $ebook = $cache->get('book_info_' . $hash);
+    if ($ebook === false || is_null($ebook)) {
+        $api_limit = 100;
+        if (!empty($_ENV['GOOGLE_API_KEY'])) {
+            $api_limit = 1000;
+        }
+        if ($api_hits >= $api_limit) {
+            if ($CURUSER['class'] >= UC_STAFF) {
+                return [
+                    "
+                <div class='padding10'>
+                    <div class='columns'>
+                        <div class='has-text-red column is-2 size_5 padding5'>Google API: </div>
+                        <span class='column padding5'>API Limit exceeded: $api_hits / $api_limit</span>
+                    </div>
+                </div>",
+                    '',
+                ];
+            }
+            return false;
+        } else {
+            date_default_timezone_set('America/Los_Angeles');
+            $secs = strtotime('tomorrow 00:00:00') - TIME_NOW;
+            $cache->increment('google_api_limits_', 1, 0, $secs);
+        }
+
         if (!empty($_ENV['GOOGLE_API_KEY'])) {
             $books = new GoogleBooks(['key' => $_ENV['GOOGLE_API_KEY']]);
         } else {
             $books = new GoogleBooks();
         }
-
         if (!empty($torrent['isbn'])) {
             $book = $books->volumes->byIsbn($torrent['isbn']);
         } else {
             $book = $books->volumes->firstOrNull($torrent['name']);
         }
-
-        if (empty($book)) {
-            return false;
+        $keys = $ebook['authors'] = $categories = [];
+        $ebook['title'] = $book->title;
+        foreach ($book->authors as $author) {
+            $ebook['authors'][] = $author;
         }
+        $ebook['publisher'] = $book->publisher;
+        $ebook['publishedDate'] = $book->publishedDate;
+        $ebook['description'] = $book->description;
+        foreach ($book->industryIdentifiers as $industryIdentifier) {
+            foreach ($industryIdentifier as $key => $value) {
+                $keys[] = $value;
+            }
+        }
+        $ebook['isbn10'] = !empty($keys[1]) && strlen($keys[1]) === 10 ? $keys[1] : !empty($keys[3]) ? $keys[3] : '';
+        $ebook['isbn13'] = !empty($keys[3]) && strlen($keys[3]) === 13 ? $keys[3] : !empty($keys[1]) ? $keys[1] : '';
+        foreach ($book->categories as $category) {
+            $ebook['categories'][] = $category;
+        }
+        $ebook['pageCount'] = $book->pageCount;
+        $ebook['poster'] = $book->imageLinks->thumbnail;
 
-        $ebook_info = "
+        if (!empty($book)) {
+            $cache->set('book_info_' . $hash, $ebook, $site_config['expires']['book_info']);
+        }
+    }
+
+    if (empty($ebook)) {
+        return false;
+    }
+
+    $ebook_info = "
                     <div class='columns'>
                         <div class='has-text-red column is-2 size_5 padding5'>Title: </div>
-                        <span class='column padding5'>{$book->title}</span>
-                    </div>";
-        $authors = [];
-        foreach ($book->authors as $author) {
-            $authors[] = $author;
-        }
-        $ebook_info .= "
+                        <span class='column padding5'>{$ebook['title']}</span>
+                    </div>
                     <div class='columns'>
                         <div class='has-text-red column is-2 size_5 padding5'>Author: </div>
-                        <span class='column padding5'>" . implode(', ', $authors) . "</span>
-                    </div>";
-        $ebook_info .= "
+                        <span class='column padding5'>" . implode(', ', $ebook['authors']) . "</span>
+                    </div>
                     <div class='columns'>
                         <div class='has-text-red column is-2 size_5 padding5'>Published: </div>
-                        <span class='column padding5'>{$book->publishedDate}</span>
+                        <span class='column padding5'>{$ebook['publisher']}<br>{$ebook['publishedDate']}</span>
                     </div>
                     <div class='columns'>
                         <div class='has-text-red column is-2 size_5 padding5'>Description: </div>
-                        <span class='column padding5'>{$book->description}</span>
+                        <span class='column padding5'>{$ebook['description']}</span>
                     </div>";
-        $keys = [];
-        if (!empty($book->industryIdentifiers)) {
-            foreach ($book->industryIdentifiers as $industryIdentifier) {
-                foreach ($industryIdentifier as $key => $value) {
-                    $keys[] = $value;
-                }
-            }
-        }
-        if (!empty($keys)) {
-            $isbn10 = !empty($keys[1]) && strlen($keys[1]) === 10 ? $keys[1] : !empty($keys[3]) ? $keys[3] : '';
-            $isbn13 = !empty($keys[3]) && strlen($keys[3]) === 13 ? $keys[3] : !empty($keys[1]) ? $keys[1] : '';
-            $ebook_info .= "
+    if (!empty($keys)) {
+        $ebook_info .= "
                     <div class='columns'>
                         <div class='has-text-red column is-2 size_5 padding5'>ISBN 10: </div>
                         <span class='column padding5'>
-                            <a href='{$site_config['anonymizer_url']}https://www.amazon.com/gp/search/field-isbn={$isbn10}' target='_blank'>{$isbn10}</a>
+                            <a href='{$site_config['anonymizer_url']}https://www.amazon.com/gp/search/field-isbn={$ebook['isbn10']}' target='_blank'>{$ebook['isbn10']}</a>
                         </span>
                     </div>
                     <div class='columns'>
                         <div class='has-text-red column is-2 size_5 padding5'>ISBN 13: </div>
                         <span class='column padding5'>
-                            <a href='{$site_config['anonymizer_url']}https://www.amazon.com/gp/search/field-isbn={$isbn13}' target='_blank'>{$isbn13}</a>
+                            <a href='{$site_config['anonymizer_url']}https://www.amazon.com/gp/search/field-isbn={$ebook['isbn13']}' target='_blank'>{$ebook['isbn13']}</a>
                         </span>
                     </div>";
-        }
+    }
 
-        $categories = [];
-        if (!empty($books->categories)) {
-            foreach ($book->categories as $category) {
-                $categories[] = $category;
-            }
-        }
-
-        if (!empty($categories)) {
-            $ebook_info .= "
+    if (!empty($ebook['categories'])) {
+        $ebook_info .= "
                     <div class='columns'>
                         <div class='has-text-red column is-2 size_5 padding5'>Genre: </div>
-                        <span class='column padding5'>" . implode(', ', $categories) . "</span>
+                        <span class='column padding5'>" . implode(', ', $ebook['categories']) . "</span>
                     </div>";
-        }
-        if (empty($torrent['poster']) && !empty($book->imageLinks->thumbnail)) {
-            $poster = $book->imageLinks->thumbnail;
-        }
-        $cache->set('book_info_' . $hash, $ebook_info, $site_config['expires']['book_info']);
     }
+    $ebook_info .= "
+                    <div class='columns'>
+                        <div class='has-text-red column is-2 size_5 padding5'>Pages: </div>
+                        <span class='column padding5'>{$ebook['pageCount']}</span>
+                    </div>";
+
+    if ($CURUSER['class'] >= UC_STAFF) {
+        $ebook_info .= "
+                    <div class='columns'>
+                        <div class='has-text-red column is-2 size_5 padding5'>API Hits: </div>
+                        <span class='column padding5'>$api_hits</span>
+                    </div>";
+    }
+
+    $poster = '';
+    if (empty($torrent['poster']) && !empty($ebook['poster'])) {
+        $poster = $ebook['poster'];
+    }
+
     return [
         "<div class='padding10'>$ebook_info</div>",
-        $poster
+        $poster,
     ];
 }
