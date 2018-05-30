558e7e1bc839da64c2c9f0e09330a65c023c507e
darkalchemy <darkalchemy@jbmatrix.net>
2018-05-30 18:29:55 +0000

    remove: tfreak.php has no need for check_user_status since not loaded directly by user

--- tfreak.php
+++ tfreak.php
@@ -1,8 +1,5 @@
 <?php
 
-require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . 'include' . DIRECTORY_SEPARATOR . 'bittorrent.php';
-require_once INCL_DIR . 'user_functions.php';
-check_user_status();
 
 /**
  * @return mixed|string
