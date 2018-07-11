941b28e2a7a03088d7e2d6855a2f2f20a4f815ce
darkalchemy <darkalchemy@jbmatrix.net>
2018-07-11 18:59:19 +0000

    fix: missing require thanks.php

--- public/ajax/thanks.php
+++ public/ajax/thanks.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once dirname(__FILE__, 3) . DIRECTORY_SEPARATOR . 'include' . DIRECTORY_SEPARATOR . 'bittorrent.php';
+require_once INCL_DIR . 'user_functions.php';
 check_user_status();
 global $site_config, $cache, $session;
 
