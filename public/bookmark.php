9269f58efe6eb4319662307b021921b0cb52e441
darkalchemy <darkalchemy@jbmatrix.net>
2018-07-11 18:58:14 +0000

    fix: missing require bookmark.php

--- public/bookmark.php
+++ public/bookmark.php
@@ -2,6 +2,7 @@
 
 require_once dirname(__FILE__, 2) . DIRECTORY_SEPARATOR . 'include' . DIRECTORY_SEPARATOR . 'bittorrent.php';
 require_once INCL_DIR . 'user_functions.php';
+require_once INCL_DIR . 'html_functions.php';
 check_user_status();
 global $CURUSER;
 
