b59bdb9bfa943b136fc9dab1eae8d320db093563
darkalchemy <darkalchemy@jbmatrix.net>
2019-01-23 11:06:38 +0000

    update: proper fix

--- templates/1/template.php
+++ templates/1/template.php
@@ -349,7 +349,7 @@ function stdfoot($stdfoot = false)
     </a>
     <script>
         $bg_image
-        var is_12_hour = " . ($use_12_hour ? $use_12_hour : 'false') . ";
+        var is_12_hour = " . ($use_12_hour ? 1 : 0) . ";
     </script>";
 
     $htmlfoot .= "
