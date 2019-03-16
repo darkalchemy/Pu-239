ca28fb0591be26d25192ae69c6652ebb256bc8f2
jonnyboy <jonnyboy@ubu-desktop>
2019-03-16 14:37:47 -0400

    update: remove user from AJAX Chat Online when logout

--- public/logout.php
+++ public/logout.php
@@ -2,6 +2,10 @@
 
 require_once __DIR__ . '/../include/bittorrent.php';
 check_user_status();
-global $CURUSER, $site_config, $session;
+global $CURUSER, $session, $fluent;
 
+$fluent->deleteFrom('ajax_chat_online')
+    ->where('userID = ?', $CURUSER['id'])
+    ->execute();
 $session->destroy();
+
