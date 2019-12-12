d0426371177f3a1f53a5345d27e790c270fc811f
darkalchemy <darkalchemy@jbmatrix.net>
2019-12-12 06:40:10 -0500

    fix: index page user blocks not centered

--- blocks/index/active_users.php
+++ blocks/index/active_users.php
@@ -48,7 +48,7 @@ $active_users .= "
         <a id='activeusers-hash'></a>
         <div id='activeusers' class='box'>
             <div class='bordered'>
-                <div class='alt_bordered bg-00'>
+                <div class='alt_bordered bg-00 has-text-centered'>
                     <div class='bg-00 padding10 bottom10 has-text-centered round5 size_5'>
                         " . _pfe('{0} Active User', '{0} Active Users', $active['actcount']) . "
                     </div>
