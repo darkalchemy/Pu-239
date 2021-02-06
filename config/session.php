7d1896bf341cb4c2fe47fe023f87db426ddd764e
darkalchemy <darkalchemy@jbmatrix.net>
2021-02-05 22:10:24 -0500

    refactor: php ini settings

--- config/session.php
+++ config/session.php
@@ -9,6 +9,16 @@ use Pu239\Settings;
 
 global $container;
 
+// Override the settings in php.ini
+ini_set('memory_limit', '1024M');
+ini_set('zlib.output_compression', 'Off');
+ini_set('display_errors', 'Off');
+ini_set('log_errors', 'On');
+ini_set('ignore_repeated_errors', 'On');
+//ini_set('error_reporting', 'E_ALL');
+ini_set('error_log', PHPERROR_LOGS_DIR . 'error.log');
+
+// Set seession and cookies values
 $settings = $container->get(Settings::class);
 $site_config = $settings->get_settings();
 if (ini_get('session.save_handler') != 'files') {
@@ -27,13 +37,5 @@ ini_set('session.lazy_write', '0');
 ini_set('session.cookie_httponly', '1');
 ini_set('max_execution_time', '300');
 ini_set('session.cookie_domain', '');
-
-ini_set('memory_limit', '1024M');
-ini_set('zlib.output_compression', 'Off');
-ini_set('display_errors', 'Off');
-ini_set('log_errors', 'On');
-ini_set('ignore_repeated_errors', 'On');
-ini_set('error_reporting', 'E_ALL');
-ini_set('error_log', PHPERROR_LOGS_DIR . 'error.log');
 
 Session::start('Strict');
