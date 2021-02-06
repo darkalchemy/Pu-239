0ef2bfa9f20973b0358a3eebc776874ddbcaae40
darkalchemy <darkalchemy@jbmatrix.net>
2021-02-05 22:11:30 -0500

    add: PeerCache class for replacing peers table with memcached peers

---
+++ src/PeerCache.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Pu239;
+
+use MatthiasMullie\Scrapbook\Adapters\Collections\Utils\PrefixKeys;
+use MatthiasMullie\Scrapbook\Adapters\Memcached;
+use MatthiasMullie\Scrapbook\Buffered\BufferedStore;
+use MatthiasMullie\Scrapbook\Scale\StampedeProtector;
+use Psr\Container\ContainerInterface;
+
+/**
+ * Class PeerCache.
+ */
+class PeerCache extends StampedeProtector
+{
+    protected $cache;
+    protected $container;
+    protected $env;
+
+    /**
+     * Cache constructor.
+     *
+     * @param ContainerInterface $c
+     *
+     */
+    public function __construct(ContainerInterface $c)
+    {
+        $this->container = $c;
+        $this->env = $this->container->get('env');
+        if (!extension_loaded('memcached')) {
+            die('<h1>Error</h1><p>php-memcached is not available</p>');
+        }
+        $client = $this->container->get(Memcached::class);
+        $client = new PrefixKeys($client, $this->env['paar_cache']['prefix']);
+        $client = new BufferedStore($client);
+        $this->cache = new StampedeProtector($client);
+
+        parent::__construct($this->cache);
+    }
+}
